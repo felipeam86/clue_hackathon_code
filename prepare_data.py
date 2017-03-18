a35f9b1d09af8a864968b6a3f1098a1b755745d2
Felipe Aguirre Martinez <felipeam86@gmail.com>
2017-03-18 18:24:18 +0100

    First version of data sequence per user

--- prepare_data.py
+++ prepare_data.py
@@ -1,7 +1,7 @@
 from os.path import join as pj
 import os
-from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import numpy as np
 
 base_dir = os.path.dirname(__file__)
 data_dir = pj(base_dir, 'data')
@@ -19,7 +19,33 @@ tracking = pd.read_csv(pj(data_dir, 'tracking.csv'), parse_dates=['date'])
 tracking_test = pd.read_csv(pj(data_dir, 'labels.csv'))
 
 
-# ======================  ======================
-symptom_encoder = LabelEncoder()
-symptom_encoder.fit(tracking.symptom.unique())
-tracking['symptom_code'] = symptom_encoder.transform(tracking.symptom)
+# ================  Feature engineer a user ================
+symptoms_of_interest = [
+    'happy', 'pms', 'sad', 'sensitive_emotion',
+    'energized', 'exhausted', 'high_energy', 'low_energy',
+    'cramps', 'headache', 'ovulation_pain', 'tender_breasts',
+    'acne_skin', 'good_skin', 'oily_skin', 'dry_skin'
+]
+
+other_symptoms = set(tracking.symptom.unique()) - set(symptoms_of_interest)
+ordered_symptoms = {s:i for i, s in enumerate(symptoms_of_interest + list(other_symptoms))}
+N_symptoms = len(ordered_symptoms)
+
+tracking['symptom_code'] = tracking.symptom.map(lambda s: ordered_symptoms[s])
+
+
+def transform_user(user_id):
+    out = tracking[tracking.user_id == user_id]
+    out.loc[:, 'absolute_day'] = (out.date.dt.date - out.date.dt.date.min()).dt.days + 1
+    out = pd.merge(out, cycles[cycles.user_id == user_id], on=["cycle_id", "user_id"])
+
+    out_sequence = np.zeros(
+        (
+            out.absolute_day.max(),
+            N_symptoms
+        )
+    )
+
+    for _, row in out.iterrows():
+        out_sequence[row['absolute_day'] - 1, row['symptom_code']] = 1
+    return out, out_sequence
