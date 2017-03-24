428bc3a24222814cd6c0dbe185947494692c7300
Felipe Aguirre Martinez <felipeam86@gmail.com>
2017-03-24 17:46:46 +0100

    Final run.sh structure

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "starting"
-python3 /train.py -model 1 -weights weights/lstm_1_layer.hdf5
-python3 /predict.py -model 1 -weights weights/lstm_1_layer.hdf5
+echo "Training the model"
+python3 /train.py -model 1
+echo "Generating predictions"
+python3 /predict.py -model 1
